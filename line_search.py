a91dbac88a79b44adb46c8d1c16d3c7fb92603c2
James Wu <jameswu144@gmail.com>
2024-05-29 15:05:53 +0100

    steepest descent + exercise 3.1

--- line_search.py
+++ line_search.py
@@ -17,4 +17,61 @@ def backtracking_line_search(f, df, xk, pk, rho, c, alpha_bar):
         alpha *= rho
     return alpha
 
-# TODO: Program steepest descent and Newton's method
+
+def descent(f, df, x0, dir_func, tolerance=1e-8, max_iterations=5000):
+    """
+    f (func): objective function
+    df (func): gradient of f
+    x0 (array): initial point
+    dir_func (func): function obtaining descent direction
+    tolerance (float): acceptable level of error
+    max_iterations (int): maximum iterations
+    """
+
+    x = x0
+    rho = 0.5
+    c = 0.5
+    alpha_bar = 1
+
+    for _ in range(max_iterations):
+        # check if it's a local minimum by checking gradient
+        if np.linalg.norm(df(x)) < tolerance:
+            return x
+
+        # descent direction
+        p = dir_func(x)
+
+        alpha = backtracking_line_search(f, df, x, p, rho, c, alpha_bar)
+        x += alpha * p
+
+    raise ConvergenceError("Unable to find a local minimum.")
+
+
+def steepest_descent(f, df, x0):
+    def descent_direction(x):
+        return -df(x)
+    return descent(f, df, x0, descent_direction)
+
+
+class ConvergenceError(Exception):
+    pass
+
+
+
+# Exercise 3.1 (page 63)
+
+x0 = np.array([-1.2, 1]).T
+
+# here x is a np array of two elements
+def rosenbrock(x):
+    x1, x2 = x
+    return 100 * (x2 - x1**2)**2 + (1-x1)**2
+
+def d_rosenbrock(x):
+    x1, x2 = x
+    return np.array([
+        -400*x1*(x2-x1**2) - 2*(1-x1),
+        200*(x2-x1**2)
+    ])
+
+print(steepest_descent(rosenbrock, d_rosenbrock, x0))
