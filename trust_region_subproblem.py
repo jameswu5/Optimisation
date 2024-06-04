a6f0ceb9ed7939a12678847673bd3a7ae181523c
ShunHao Lin <shun.lin22@imperial.ac.uk>
2024-06-04 14:22:29 +0100

    Some corrections to trust_region_subproblem

--- trust_region_subproblem.py
+++ trust_region_subproblem.py
@@ -1,8 +1,12 @@
 import numpy as np
 
-# The algorithm is still suboptimal compared to the literature, but
-# this will do for now (especially Newton's method).
-# section 4.3
+"""
+This is to obtain a better step in each trust region iteration.
+
+The algorithm is still suboptimal compared to the literature, but
+this will do for now (especially Newton's method).
+section 4.3
+"""
 
 
 def subproblem_solve_newton(delta, B, df, lambda1, lambda0=10):
@@ -27,7 +31,7 @@ def subproblem_solve_newton(delta, B, df, lambda1, lambda0=10):
 
     # L L^T function output though R^T R is algorithm format
     # Termination criteria for future improvement
-    for i in range(5):
+    for _ in range(5):
         L = np.linalg.cholesky(B + lambda_l * np.identity(n))
         p = -np.linalg.inv(L.T) @ np.linalg.inv(L) @ df
         q = np.linalg.inv(L) @ p
@@ -41,11 +45,14 @@ def subproblem_solve_newton(delta, B, df, lambda1, lambda0=10):
             lambda_l = max(0.001*lambda_up, np.sqrt(lambda_low*lambda_up))
 
         # update safeguards, lambda_s update future improvement
-        if lambda_l > -lambda1 and (1/delta - 1/p < 0):
+        if lambda_l > -lambda1 and (1/delta - 1/np.linalg.norm(p) < 0):
             lambda_up = min(lambda_up, lambda_l)
         else:
             lambda_low = max(lambda_low, lambda_l)
         lambda_low = max(lambda_low, lambda_s)
+
+    L = np.linalg.cholesky(B + lambda_l * np.identity(n))
+    return -np.linalg.inv(L.T) @ np.linalg.inv(L) @ df
 
 
 def subproblem_hard(delta, df, e_val, e_vec):
@@ -78,7 +85,7 @@ def subproblem_solve(delta, B, df, lambda0=10):
     # simple case, B positive definite
     try:
         L = np.linalg.cholesky(B)
-    except LinAlgError:
+    except np.linalg.LinAlgError:
         pass
     else:
         p = np.linalg.inv(L.T) @ np.linalg.inv(L) @ df
@@ -91,7 +98,3 @@ def subproblem_solve(delta, B, df, lambda0=10):
         return subproblem_hard(delta, df, e_val, e_vec)
 
     return subproblem_solve_newton(delta, B, df, e_val[0], lambda0)
-
-
-class LinAlgError(Exception):
-    pass
