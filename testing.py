2a21464001515b517e2b761dc88a12b8a049bf6e
James Wu <jameswu144@gmail.com>
2024-06-05 22:15:46 +0100

    ackley finally works

--- testing.py
+++ testing.py
@@ -59,17 +59,16 @@ def rastrigin_testing():
     print(eq)
     print(eq2)
 
-# This one still doesn't work
+# We test this one on BFGS
 def ackley_testing():
     a = Descent(ackley)
 
-    x0 = np.array([-0.1, 0.5])
-    # eq = a.descend(x0, a.steepest, backtracking, display=True)
+    # x0 = np.array([1.9, 3.5]) # converges to [2.97313942 1.98213995]
+    x0 = np.array([0.2, -0.7]) # converges to [ 1.44368052e-09 -9.52166544e-01]
     eq = a.BFGS(x0)
-
-    # print(ackley.derivative(np.array([-8.62621316e-17, -8.36234119e-17])))
-
     print(eq)
+
+    # print(ackley.derivative(np.array([ 1.44368052e-09, -9.52166544e-01])))
 
 
 def himmelblau_plot():
