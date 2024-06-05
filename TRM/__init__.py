931290598027e427b5815c9b48feb9cf3428986b
Jiayi Gao <jg2622@ic.ac.uk>
2024-06-05 04:58:12 +0100

    1

---
+++ TRM/__init__.py
@@ -0,0 +1,1 @@
+from .trm import cauchy, trm_cauchy, dogleg, trm_dogleg, subspace, trm_subspace  # noqa F401
---
+++ TRM/trm.py
@@ -0,0 +1,205 @@
+import numpy as np
+from autograd import grad, hessian
+
+
+def cauchy(f, x, delta):
+    grad_f = grad(f)
+    hess_f = hessian(f)
+    g = grad_f(x)
+    B = hess_f(x)
+
+    # Calculate p_k^s
+    pks = - (delta / np.linalg.norm(g)) * g
+
+    # Calculate tau_k
+    gBg = np.dot(g.T, np.dot(B, g))
+    if gBg <= 0:
+        tau_k = 1
+    else:
+        tau_k = min(np.linalg.norm(g)**3 / (delta * gBg), 1)
+
+    # Calculate p_k^C
+    pkc = tau_k * pks
+
+    return pkc
+
+
+def trm_cauchy(f, x0, delta0, delta_max, eta, iter_time, tolerance):
+    # Initialize
+    x = x0
+    delta = delta0
+    # Iterate using cauchy point calculation
+    for k in range(iter_time):
+        p = cauchy(f, x, delta)
+        ar = f(x) - f(x + p)
+        pr = -f(x) + (-f(x) + np.dot(np.gradient(f, x), p))
+        rho = ar / pr
+
+        # Adjust the trust region radius
+        if rho < 0.25:
+            delta = 0.25 * delta
+        elif rho > 0.75 and np.linalg.norm(p) == delta:
+            delta = min(2 * delta, delta_max)
+        else:
+            delta = delta
+
+        # Update the iteration point
+        if rho > eta:
+            x = x + p
+        else:
+            x = x
+
+        # Check if the current solution closed enough to the optimal solution
+        if np.linalg.norm(np.gradient(f, x)) < tolerance:
+            break
+
+    return x
+
+
+def dogleg(f, x, delta):
+    grad_f = grad(f)
+    hess_f = hessian(f)
+    g = grad_f(x)
+    B = hess_f(x)
+
+    # Calculate Cauchy point
+    p_u = cauchy(f, x, delta)
+
+    # Compute Newton direction
+    try:
+        p_b = - np.linalg.solve(B, g)
+        if np.any(np.linalg.eigvals(B) <= 0):
+            raise np.linalg.LinAlgError
+    except np.linalg.LinAlgError:
+        p_b = p_u
+
+    # Solve depending on the trust region constraint
+    if np.linalg.norm(p_b) <= delta:
+        return p_b
+    elif np.linalg.norm(g) > delta:
+        return - g * delta / np.linalg.norm(g)
+    else:  # Solve for intermediate values of delta
+        u = p_u
+        v = p_b - p_u
+        tau = (-np.dot(u, v) + np.sqrt(np.dot(u, v)**2 + np.dot(v, v) * (delta**2 - np.dot(u, u)))) / np.dot(v, v)
+        if 0 <= tau <= 1:
+            return tau * u
+        elif 1 <= tau <= 2:
+            return u + (tau - 1) * v
+
+
+def trm_dogleg(f, x0, delta0, delta_max, eta, iter_time, tolerance):
+    # Initialize
+    x = x0
+    delta = delta0
+
+    # Iterate using dogleg method
+    for k in range(iter_time):
+        p = dogleg(f, x, delta)
+        ar = f(x) - f(x + p)
+        pr = -f(x) + (-f(x) + np.dot(np.gradient(f, x), p))
+        rho = ar / pr
+
+        # Adjust the trust region radius
+        if rho < 0.25:
+            delta = 0.25 * delta
+        elif rho > 0.75 and np.linalg.norm(p) == delta:
+            delta = min(2 * delta, delta_max)
+        else:
+            delta = delta
+
+        # Update the iteration point
+        if rho > eta:
+            x = x + p
+        else:
+            x = x
+
+        # Check if the current solution closed enough to the optimal solution
+        if np.linalg.norm(np.gradient(f, x)) < tolerance:
+            break
+
+    return x
+
+
+def subspace(f, x, delta):
+    grad_f = grad(f)
+    hess_f = hessian(f)
+    g = grad_f(x)
+    B = hess_f(x)
+
+    # Use the subspace span[g, B^{-1}g]
+    if np.all(np.linalg.eigvals(B) > 0):
+        try:
+            p_b = -np.linalg.solve(B, g)
+        except np.linalg.LinAlgError:
+            p_b = -g
+        if np.linalg.norm(p_b) <= delta:
+            return p_b
+
+    # Use the subspace span[g, (B + alpha*I)^{-1}g]
+    if np.any(np.linalg.eigvals(B) < 0):
+        lambda1 = np.min(np.linalg.eigvals(B))
+        alpha = -lambda1 * 1.5
+
+        try:
+            p_b = -np.linalg.solve(B + alpha * np.eye(len(B)), g)
+        except np.linalg.LinAlgError:
+            p_b = -g
+
+        # Calculate the vector v
+        if np.linalg.norm(p_b) <= delta:
+            return p_b
+        v = delta * (p_b / np.linalg.norm(p_b)) - p_b
+        v = v - np.dot(v, np.linalg.solve(B + alpha * np.eye(len(B)), g)) * np.linalg.solve(B + alpha * np.eye(len(B)), g)
+
+        # Update the step p
+        p = p_b + v
+        if np.linalg.norm(p) > delta:
+            p = (delta / np.linalg.norm(p)) * p
+
+        return p
+
+    # Define the step to be the Cauchy point
+    elif np.all(np.linalg.eigvals(B) >= 0) and np.any(np.linalg.eigvals(B) == 0):
+        return cauchy(g, B, delta)
+
+    else:
+        p_u = - (np.dot(g.T, g) / np.dot(g.T, np.dot(B, g))) * g
+        u = p_u
+        v = p_b - p_u
+        tau = (-np.dot(u, v) + np.sqrt(np.dot(u, v)**2 + np.dot(v, v) * (delta**2 - np.dot(u, u)))) / np.dot(v, v)
+        if 0 <= tau <= 1:
+            return tau * u
+        elif 1 <= tau <= 2:
+            return u + (tau - 1) * v
+
+
+def trm_subspace(f, x0, delta0, delta_max, eta, iter_time, tolerance):
+    x = x0
+    delta = delta0
+
+    for k in range(iter_time):
+        grad_f = grad(f)
+        hess_f = hessian(f)
+        p = subspace(f, x, delta)
+        if np.linalg.norm(p) > delta:
+            p = p * (delta / np.linalg.norm(p))
+        ar = f(x) - f(x + p)
+        pr = -f(x) + (-f(x) + np.dot(grad_f(x), p)
+                      + 0.5 * np.dot(p, np.dot(hess_f(x), p)))
+        rho = ar / pr
+
+        if rho < 0.25:
+            delta = 0.25 * delta
+        elif rho > 0.75 and np.linalg.norm(p) == delta:
+            delta = min(2 * delta, delta_max)
+        else:
+            delta = delta
+
+        if rho > eta:
+            x = x + p
+
+        if np.linalg.norm(grad_f(x)) < tolerance:
+            break
+
+    return x
